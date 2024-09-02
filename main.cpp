8c7d843fcf88aaab1133bb2e94c988280727e83a
Robert Oprean <robert.oprean10@gmail.com>
2024-09-02 13:40:35 +0300

    Add choseDifficulty

--- main.cpp
+++ main.cpp
@@ -2,7 +2,37 @@
 
 using namespace std;
 
-int main(){
+int SIZE, MINES;
+
+void choseDifficulty()
+{
+    int level;
+
+    cout << "Chose the difficulty:" << '\n';
+    cout << "Press 0 for EASY 9x9" << '\n';
+    cout << "Press 1 for MEDIUM 16x16" << '\n';
+    cout << "Press 2 for HARD 24x24" << '\n';
+
+    cin >> level;
+    if (level == 0)
+    {
+        SIZE = 9;
+        MINES = 10;
+    }
+    if (level == 1)
+    {
+        SIZE = 16;
+        MINES = 40;
+    }
+    if (level == 2)
+    {
+        SIZE = 24;
+        MINES = 99;
+    }
+}
+
+int main()
+{
 
     return 0;
 }
